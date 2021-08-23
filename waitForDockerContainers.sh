ce3e6b82dbbded36b47041ac2327e1a571e462c8
Herwig <herwig.guggi@htl-klu.at>
2021-08-23 22:31:01 +0200

    added action and modified shell

--- waitForDockerContainers.sh
+++ waitForDockerContainers.sh
@@ -8,8 +8,8 @@ while [ $NUM_CONTAINERS_STARTING != 0 ]
 do
 printf '\e[1;34m%-6s\e[m\n\n' "$NUM_CONTAINERS_STARTING containers are still starting"
 docker ps
-printf "waiting for 10 seconds ...\n\n"
-sleep 10
+printf "waiting for $1 seconds ...\n\n"
+sleep $1
 NUM_CONTAINERS_STARTING="$(docker ps | grep '(health: starting)' | wc -l)"
 done
 printf '\e[1;34m%-6s\e[m\n' "no container in state (health: starting) ..."
